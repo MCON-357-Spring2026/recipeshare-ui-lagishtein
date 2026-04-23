9bc751a13ef7b1f4fa0504a6a9b1a70b12efc65d
Lila Agishtein <lagishtein@gmail.com>
2026-04-22 21:16:55 -0400

    Add to git my changes for the lesson 1

---
+++ recipes-frontend/app/page.js
@@ -0,0 +1,10 @@
+import RecipeList from "@/components/RecipeList";
+
+export default function HomePage() {
+  return (
+    <main>
+      <h1>My Recipes</h1>
+      <RecipeList />
+    </main>
+  );
+}
